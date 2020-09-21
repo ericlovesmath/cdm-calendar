41ea7ddb9b2a204a3b03e6311b2c80bc4bad7519
ericlovesmath <ericlovesmath@gmail.com>
2020-09-20 23:38:42 -0700

    Removes dependancies, added comments

--- cdm-calendar.py
+++ cdm-calendar.py
@@ -1,48 +1,52 @@
-import requests
-import os
-from bs4 import BeautifulSoup
-import webbrowser
-import pdftotext
-from datetime import datetime,timedelta
-import time
-import json
-import sys
-import re
+import requests, pdftotext                  # Scrapes website to pdf
+from bs4 import BeautifulSoup               # Parses HTML
+import os, sys                              # Changes paths and checks args
+import webbrowser                           # Opens URL
+from datetime import datetime, timedelta    # Gets dates and time
 
-page = requests.get("http://cdm.nmusd.us")
-soup = BeautifulSoup(page.content, 'html.parser')
-cal = soup.body.div("a", string="Printable")[0].get("href")
+page = requests.get("http://cdm.nmusd.us")                   # Scrapes CDM website
+soup = BeautifulSoup(page.content, 'html.parser')            # Parses html
+cal = soup.body.div("a", string="Printable")[0].get("href")  # Gets hyperlink named "Printable"
 
+# Writes cal.pdf and reads it
 with requests.get(cal) as r:
-    with open("cal.pdf", "wb") as f:
+    with open(os.path.expanduser("~/Desktop/Bash/cal.pdf"), "wb") as f:
         f.write(r.content)
 
-print("\n"*10)
-
-with open("cal.pdf", "rb") as f:
+with open(os.path.expanduser("~/Desktop/Bash/cal.pdf"), "rb") as f:
     pdf = pdftotext.PDF(f, "secret")
 
-month = int(time.strftime('%m'))
-day = int(time.strftime('%d'))
-
-dayOfWeek = time.strftime('%A')
+# Today's day of the week
+date = datetime.today()
+dayOfWeek = date.strftime('%A')
 weekDayNum = datetime.today().weekday()
 
-startWeekMonth = int((datetime.today() + timedelta(days=(-weekDayNum))).strftime('%m'))
-startWeekDay = int((datetime.today() + timedelta(days=(-weekDayNum))).strftime('%d'))
+# Date of today
+month = int(date.strftime('%m'))
+day = int(date.strftime('%d'))
+today = f"{month}/{day}"
 
-endWeekMonth = int((datetime.today() + timedelta(days=(5 - weekDayNum))).strftime('%m'))
-endWeekDay = int((datetime.today() + timedelta(days=(5 - weekDayNum))).strftime('%d'))
+# Date of start of school week
+startWeekMonth = int( (date + timedelta(days=(-weekDayNum))).strftime('%m') ) 
+startWeekDay = int( (date + timedelta(days=(-weekDayNum))).strftime('%d') )
+startDay = f"{startWeekMonth}/{startWeekDay}"
 
-timeText = f"{startWeekMonth}/{startWeekDay}-{endWeekMonth}/{endWeekDay}"
+# Date of end of school week
+endWeekMonth = int( (date + timedelta(days=(5 - weekDayNum))).strftime('%m') )
+endWeekDay = int( (date + timedelta(days=(5 - weekDayNum))).strftime('%d') )
+endDay = f"{endWeekMonth}/{endWeekDay}"
 
+# Prints schedule and replaces key items with colored text
 print("      " + pdf[0]
-    .replace("\n", "\n      ")
-    .replace(timeText, f"\033[0;36m{timeText}\033[0m")
-    .replace(dayOfWeek,f"\033[0;36m{dayOfWeek}\033[0m")
+    .replace("\n", "\n      ")                             # Adds space to front of each line
+    .replace(today, f"\033[1;31m{today}\033[0m")           # Color today's date red
+    .replace(startDay, f"\033[0;36m{startDay}\033[0m")     # Color the start of the week cyan
+    .replace(endDay, f"\033[0;36m{endDay}\033[0m")         # Color the end of the week cyan
+    .replace(dayOfWeek,f"\033[0;36m{dayOfWeek}\033[0m")    # Color the day of the week cyan
     )
 
-print(f"Current Week Dates: {timeText}, Current Date: {month}/{day}")
+print(f"Current Week Dates: {startDay} - {endDay}, Current Date: {month}/{day}, Day of the Week: {dayOfWeek}")
 
+# Opens calendar in default browser if there are extra args
 if len(sys.argv) >= 2:
     webbrowser.open(cal)
